cd29b961a5fd72ef7efdc7acd86f109b988fe123
John Kurre <devops@natresearch.net>
2023-10-19 05:31:29 -0600

    added Clerk Authentication

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,30 @@
 import { Toaster } from 'react-hot-toast';
 import { QueryClient, QueryClientProvider } from 'react-query';
-
 import { appWithTranslation } from 'next-i18next';
 import type { AppProps } from 'next/app';
 import { Inter } from 'next/font/google';
-
 import '@/styles/globals.css';
+import { ClerkProvider, useUser } from '@clerk/nextjs';
 
 const inter = Inter({ subsets: ['latin'] });
 
-import { ClerkProvider } from "@clerk/nextjs";
-
-function App({ Component, pageProps }: AppProps<{}>) {
+function MyApp({ Component, pageProps }: AppProps<{}>) {
   const queryClient = new QueryClient();
+  const { isLoaded, isSignedIn, user } = useUser();
+
+  // In case the user is not signed in or if the user data is not loaded yet
+  if (!isLoaded || !isSignedIn) {
+    return null;
+  }
 
   return (
     <div className={inter.className}>
       <Toaster />
       <QueryClientProvider client={queryClient}>
         <ClerkProvider {...pageProps}>
+          <div>
+            Hello, {user.firstName} welcome to Clerk
+          </div>
           <Component {...pageProps} />
         </ClerkProvider>
       </QueryClientProvider>
@@ -26,4 +32,4 @@ function App({ Component, pageProps }: AppProps<{}>) {
   );
 }
 
-export default appWithTranslation(App);
+export default appWithTranslation(MyApp);
