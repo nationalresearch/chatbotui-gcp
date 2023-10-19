5cf7bfdbb00ea990185eead0f530488dc4c15dee
John Kurre <devops@natresearch.net>
2023-10-19 05:46:12 -0600

    added Clerk Authentication - corrected errors and removed ./UserContext

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,18 +4,17 @@ import { appWithTranslation } from 'next-i18next';
 import type { AppProps } from 'next/app';
 import { Inter } from 'next/font/google';
 import '@/styles/globals.css';
-import { ClerkProvider, useAuth } from '@clerk/nextjs';
-import { useUser } from '@clerk/nextjs';
+import { ClerkProvider, useAuth, useUser } from '@clerk/nextjs';
 
 const inter = Inter({ subsets: ['latin'] });
 
 function MyApp({ Component, pageProps }: AppProps<{}>) {
   const queryClient = new QueryClient();
-  const { isLoaded, userId, sessionId } = useAuth();
+  const { isLoaded: isAuthLoaded, userId, sessionId, getToken } = useAuth();
   const { isLoaded: isUserLoaded, isSignedIn, user } = useUser();
 
   // In case the user signs out while on the page or if the auth or user data is not loaded yet
-  if (!isLoaded || !isUserLoaded || !isSignedIn) {
+  if (!isAuthLoaded || !isUserLoaded || !isSignedIn) {
     return null;
   }
 
