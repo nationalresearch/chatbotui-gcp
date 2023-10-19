d1691f9e91d02338a743c2081bd9080c8985ee0c
John Kurre <devops@natresearch.net>
2023-10-19 06:29:28 -0600

    Updated temp configuration for parameterization

--- pages/middleware.ts
+++ pages/middleware.ts
@@ -1,15 +1,16 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware as clerkAuthMiddleware } from '@clerk/clerk-sdk-node';
+import { NextApiRequest, NextApiResponse } from 'next';
 
-// This example protects all routes including api/trpc routes
-// Please edit this to allow other routes to be public as needed.
-// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
+// Specify the API routes you want to exclude from auth middleware
+const excludedRoutes = ['/api/route1', '/api/route2'];
 
-export const config = {
-  api: {
-    externalResolver: true,
-  },
+export const authMiddleware = (handler) => async (req: NextApiRequest, res: NextApiResponse) => {
+    // Use Clerk middleware unless it's an excluded route
+    if (!excludedRoutes.includes(req.url!)) {
+        return clerkAuthMiddleware()(req, res, handler);
+    } else {
+        return handler(req, res);
+    }
 };
 
-const matcher = ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'];
-
-export default authMiddleware({ matcher });
+export default authMiddleware;
