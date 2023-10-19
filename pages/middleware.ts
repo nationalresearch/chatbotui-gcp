dc6118dc5348baf550459ecb3453ae0cb24955dd
John Kurre <devops@natresearch.net>
2023-10-19 06:33:11 -0600

    Updated temp configuration for parameterization

--- pages/middleware.ts
+++ pages/middleware.ts
@@ -1,16 +1,7 @@
-import { authMiddleware as clerkAuthMiddleware } from '@clerk/clerk-sdk-node';
-import { NextApiRequest, NextApiResponse } from 'next';
+import { withSession } from '@clerk/clerk-sdk-node/nextjs';
 
-// Specify the API routes you want to exclude from auth middleware
-const excludedRoutes = ['/api/route1', '/api/route2'];
-
-export const authMiddleware = (handler) => async (req: NextApiRequest, res: NextApiResponse) => {
-    // Use Clerk middleware unless it's an excluded route
-    if (!excludedRoutes.includes(req.url!)) {
-        return clerkAuthMiddleware()(req, res, handler);
-    } else {
-        return handler(req, res);
-    }
-};
-
-export default authMiddleware;
+// Here's a Next.js API Route
+export default withSession((req, res) => {
+  const { session } = req;
+  // You now have access to `session` in your API Route.
+});
